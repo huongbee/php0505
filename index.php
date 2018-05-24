e9ab6f0917833cb00c9bc0ae27f47fdc3e83c2f0
Huong <admin@Apples-MacBook-Pro.local>
2018-05-24 19:21:30 +0700

    if-else for

--- index.php
+++ index.php
@@ -22,6 +22,7 @@
 
 // $c = true;
 // echo gettype($c);
+/*
 $array = [
     12,
     2345,
@@ -46,4 +47,62 @@ class A{
 
 $a = new A;
 echo gettype($a);
+
+*/
+
+// define('PI', 3.14);
+
+// define('Author', 'Tui');
+
+// echo PI;
+
+
+$a = 1;
+//echo $a++;   //1  
+// echo ++$a;   //2
+// echo $a;     //2
+
+$a+=$a; 
+echo $a; //2 $a = $a + $a
+
+echo "<br>";
+echo $a+=5; //
+echo "<br>";
+
+
+echo $a-=3; // 4   
+
+echo "<br>";
+
+echo $a/=2; //2     *=    %= 
+
+
+echo $a%=2;  // 0     .= 
+echo "<br>";
+
+
+$b = 9;
+$c = 10;
+echo '<h2>Tong cua hai so $b + $c la: '.($b+$c)."</h2>";
+echo "<br>";
+
+switch(9){
+    case 1: echo 1;break;
+    case 2: echo 2;break;
+    default: echo 3; break;
+}
+echo "<br>";
+
+for($i = 1; $i<=10; $i++){
+    echo $i .' ';
+}
+
 ?>
+
+
+<h4>1234567</h4>
+<script>
+
+    //alert(4)
+
+</script>
