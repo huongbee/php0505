408d3437054434a11979207a01da8f21e209e134
Huong <admin@Apples-MacBook-Pro.local>
2018-05-24 18:40:50 +0700

    var

--- index.php
+++ index.php
@@ -1,5 +1,49 @@
 <?php
 
+// echo "Hello PHP";
 
+// if(1>2){
+//     echo "yes";
+// }
+// else{
+//     echo "no";
+// }
+// $a = "Hello Ti";
+// echo $a;
 
+// $b = 12;
+// echo $b;
+
+/**
+ * d2erq
+ * we
+ * 
+*/
+
+// $c = true;
+// echo gettype($c);
+$array = [
+    12,
+    2345,
+    'huong',
+    [
+        1234,
+        345
+    ]
+];
+
+$arr = [
+    'key'=>'huong'
+];
+
+$json = json_encode($arr);
+echo $json;
+// echo gettype($json);
+
+class A{
+    var $name = "huong";
+}
+
+$a = new A;
+echo gettype($a);
 ?>
